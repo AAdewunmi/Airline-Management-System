d3c27fefeab072369704f22b2b9852ca3e543b02
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-04-11 09:18:31 +0100

    Tenth Commit: Implement conn class which handles database connection

--- src/airline/conn.java
+++ src/airline/conn.java
@@ -8,6 +8,33 @@ package airline;
  *
  * @author adrianadewunmi
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author adrianadewunmi
+ */
 public class conn {
+    
+    Connection c;
+    Statement s;
+    
+    public conn(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/ams", "root", "abc");
+            s = c.createStatement();
+        } catch (SQLException e) {
+            System.out.println("Connection Error: " + e);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(conn.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     
 }
